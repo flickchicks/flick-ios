bca95de43716fdaed504c1b947cd95200355deee
lucyxubroad <llx2@cornell.edu>
2020-05-22 04:42:35 -0400

    Add Flick XCode files

---
+++ Flick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flick
+//
+//  Created by Lucy Xu on 5/22/20.
+//  Copyright © 2020 flick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
