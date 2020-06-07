1f24748d27a002ede9baed5054da16390b636f62
haiyingweng <haiyingweng@gmail.com>
2020-06-07 14:24:55 -0400

    Re-setup collection view and add list summary

--- Flick/Views/MediaListHeaderView.swift
+++ Flick/Views/MediaListHeaderView.swift
@@ -74,7 +74,7 @@ class MediaListHeaderView: UICollectionReusableView {
         }
 
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
