baddf091cf97ab05c2649048ebe54162a1b5152c
lucyxubroad <llx2@cornell.edu>
2020-06-20 12:59:41 -0400

    Make modal view file

---
+++ Flick/Views/ModalView.swift
@@ -0,0 +1,23 @@
+//
+//  ModalView.swift
+//  Flick
+//
+//  Created by Lucy Xu on 6/20/20.
+//  Copyright © 2020 flick. All rights reserved.
+//
+
+import UIKit
+
+class ModalView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+
+}
