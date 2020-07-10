d84db409508c9992a714121a5f1643840f3daa3a
Lucy Xu <llx2@cornell.edu>
2020-07-10 17:17:09 -0400

    Add custom pulley card view  (#45)
    * Create initial media view controller
    
    * Add slide up card view
    
    * Add save button
    
    * Clean up code
    
    * Add button image and fix dimensions
    
    * Address PR comments
    
    * Make media card view controller rounded on top only

---
+++ Flick/Controllers/MediaCardViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MediaCardViewController.swift
+//  Flick
+//
+//  Created by Lucy Xu on 7/7/20.
+//  Copyright © 2020 flick. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum CardState { case expanded, collapsed }
+
+class MediaCardViewController: UIViewController {
+
+    // MARK: - Private View Vars
+    let handleArea = UIView()
+    let handleIndicatorView = UIView()
+
+    // MARK: - Private Data Vars
+    let handleIndicatorViewSize = CGSize(width: 64, height: 5)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .movieWhite
+        view.layer.cornerRadius = 36
+        // Apply corner radius only to top left and bottom right corners
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+
+        setupHandleArea()
+        
+    }
+
+    private func setupHandleArea() {
+        view.addSubview(handleArea)
+
+        handleIndicatorView.layer.backgroundColor = UIColor.lightGray4.cgColor
+        handleIndicatorView.layer.cornerRadius = 2
+        view.addSubview(handleIndicatorView)
+
+        handleArea.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(75)
+        }
+
+        handleIndicatorView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.size.equalTo(handleIndicatorViewSize)
+            make.top.equalToSuperview().offset(12)
+        }
+    }
+
+}
---
+++ Flick/Controllers/MediaViewController.swift
@@ -0,0 +1,167 @@
+//
+//  MediaViewController.swift
+//  Flick
+//
+//  Created by Lucy Xu on 7/7/20.
+//  Copyright © 2020 flick. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MediaViewController: UIViewController {
+
+    // MARK: - Private View Vars
+    private var mediaCardViewController: MediaCardViewController!
+    private let mediaImageView = UIImageView()
+    private let saveMediaButton = UIButton()
+    private var visualEffectView: UIVisualEffectView!
+
+    // MARK: - Private Data Vars
+    private var animationProgressWhenInterrupted: CGFloat = 0
+    private let buttonSize = CGSize(width: 72, height: 72)
+    private var cardExpanded = false
+    private var expandedCardHeight: CGFloat!
+    private var collapsedCardHeight: CGFloat!
+    private var mediaImageHeight: CGFloat!
+    private var nextState: CardState {
+        return cardExpanded ? .collapsed : .expanded
+    }
+    private var runningAnimations = [UIViewPropertyAnimator]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        expandedCardHeight = 0.9 * view.frame.height
+        collapsedCardHeight = 0.4 * view.frame.height
+        mediaImageHeight = 0.6 * view.frame.height
+
+        setupNavigationBar()
+
+        mediaImageView.image = UIImage(named: "dunkirk")
+        mediaImageView.contentMode = .scaleAspectFill
+        view.addSubview(mediaImageView)
+
+        mediaImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaInsets.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(mediaImageHeight)
+        }
+
+        setupMediaCard()
+    }
+
+    private func setupNavigationBar() {
+        let backButtonSize = CGSize(width: 22, height: 18)
+
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+
+        let backButton = UIButton()
+        backButton.setImage(UIImage(named: "whiteBackArrow"), for: .normal)
+        backButton.snp.makeConstraints { make in
+            make.size.equalTo(backButtonSize)
+        }
+
+        backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
+        let backBarButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.leftBarButtonItem = backBarButtonItem
+    }
+
+    private func setupMediaCard() {
+        visualEffectView = UIVisualEffectView()
+        visualEffectView.frame = self.view.frame
+        view.addSubview(visualEffectView)
+
+        mediaCardViewController = MediaCardViewController()
+        addChild(mediaCardViewController)
+        view.addSubview(mediaCardViewController.view)
+
+        saveMediaButton.frame = CGRect(x: self.view.frame.width - 68 - buttonSize.width/2,
+                                       y: self.view.frame.height - collapsedCardHeight - buttonSize.width/2,
+                                       width: buttonSize.width, height: buttonSize.height)
+        saveMediaButton.setImage(UIImage(named: "saveButton"), for: .normal)
+        saveMediaButton.layer.cornerRadius = buttonSize.width / 2
+        saveMediaButton.addTarget(self, action: #selector(saveMedia), for: .touchUpInside)
+        view.addSubview(saveMediaButton)
+
+        mediaCardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - collapsedCardHeight, width: self.view.bounds.width, height: expandedCardHeight)
+        mediaCardViewController.view.clipsToBounds = true
+
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleCardPan))
+        mediaCardViewController.handleArea.addGestureRecognizer(panGestureRecognizer)
+        
+    }
+
+    @objc func backButtonPressed() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc func saveMedia() {
+        print("Save media.")
+    }
+
+    @objc func handleCardPan(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            startInteractiveTransition(state: nextState, duration: 0.7)
+        case .changed:
+            let translation = recognizer.translation(in: self.mediaCardViewController.handleArea)
+            var fractionComplete = translation.y / expandedCardHeight
+            fractionComplete = cardExpanded ? fractionComplete : -fractionComplete
+            updateInteractiveTransiton(fractionCompleted: fractionComplete)
+        case .ended:
+            continueInteractiveTransition()
+        default:
+            break
+        }
+    }
+
+    private func animateTransitionIfNeeded(state: CardState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .expanded:
+                    self.mediaCardViewController.view.frame.origin.y = self.view.frame.height - self.expandedCardHeight
+                    self.saveMediaButton.frame.origin.y = self.mediaCardViewController.view.frame.origin.y - self.buttonSize.width/2
+                case .collapsed:
+                    self.mediaCardViewController.view.frame.origin.y = self.view.frame.height - self.collapsedCardHeight
+                    self.saveMediaButton.frame.origin.y = self.mediaCardViewController.view.frame.origin.y - self.buttonSize.width/2
+                }
+            }
+
+            frameAnimator.addCompletion { _ in
+                self.cardExpanded = !self.cardExpanded
+                self.runningAnimations.removeAll()
+            }
+
+            frameAnimator.startAnimation()
+            runningAnimations.append(frameAnimator)
+
+        }
+    }
+
+    private func startInteractiveTransition(state: CardState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            animateTransitionIfNeeded(state: state, duration: duration)
+        }
+        for animator in runningAnimations {
+            animator.pauseAnimation()
+            animationProgressWhenInterrupted = animator.fractionComplete
+        }
+    }
+
+    private func updateInteractiveTransiton(fractionCompleted: CGFloat) {
+        for animator in runningAnimations {
+            animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+        }
+    }
+
+    private func continueInteractiveTransition() {
+        for animator in runningAnimations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
+    }
+
+}
