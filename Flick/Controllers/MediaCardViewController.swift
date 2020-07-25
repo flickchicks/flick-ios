4865fa50089d915d9cb514ee84edb6f2a96bbda7
lucyxubroad <llx2@cornell.edu>
2020-07-25 18:32:36 -0400

    Fix scroll view

--- Flick/Controllers/MediaCardViewController.swift
+++ Flick/Controllers/MediaCardViewController.swift
@@ -16,27 +16,23 @@ class MediaCardViewController: UIViewController {
     // MARK: - Private View Vars
     let handleArea = UIView()
     private let handleIndicatorView = UIView()
-
-    // TODO: Initialize with media summary information
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     private var summaryView: MediaSummaryView!
     private var reviewView: MediaReviewView!
-
-     let rangeSlider = RangeSlider(frame: .zero)
-    let rangeSlider2 = RangeSlider(frame: .zero)
-
     private let criticRatingLabel = UILabel()
     private let friendRatingLabel = UILabel()
-
-    // MARK: - Private Data Vars
-    private let handleIndicatorViewSize = CGSize(width: 64, height: 5)
-
+    private let rangeSlider = RangeSlider(frame: .zero)
+    private let rangeSlider2 = RangeSlider(frame: .zero)
     private let commentView = UIView()
     private let commentSeparatorView = UIView()
     private let commentTextField = UITextField()
     private let sendCommentButton = UIButton()
-
     private var commentsTableView: UITableView!
 
+
+    // MARK: - Private Data Vars
+    private let handleIndicatorViewSize = CGSize(width: 64, height: 5)
     private let comments = [
         Comment(name: "Haiying W", comment: "OMG best movie ever!!! I luv all the characters hehehe OMG best movie ever!!! I luv all the characters hehehe", date: "1d"),
         Comment(name: "Aastha S", comment: "Test comment", date: "3d"),
@@ -50,43 +46,59 @@ class MediaCardViewController: UIViewController {
         // Apply corner radius only to top left and bottom right corners
         view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
 
-        setupHandleArea()
+        view.addSubview(handleArea)
+        handleIndicatorView.layer.backgroundColor = UIColor.lightGray4.cgColor
+        handleIndicatorView.layer.cornerRadius = 2
+        view.addSubview(handleIndicatorView)
+
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.isScrollEnabled = true
+        scrollView.bounces = true
+        scrollView.frame = scrollView.bounds
+        view.addSubview(scrollView)
+
+        scrollView.addSubview(contentView)
+        contentView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
 
         summaryView = MediaSummaryView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 0))
         summaryView.sizeToFit()
-        view.addSubview(summaryView)
+        contentView.addSubview(summaryView)
 
         reviewView = MediaReviewView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 0))
-        view.addSubview(reviewView)
+        contentView.addSubview(reviewView)
 
-        print(summaryView.intrinsicContentSize.height)
+        rangeSlider.addTarget(self, action: #selector(rangeSliderValueChanged(_:)), for: .valueChanged)
+        let time = DispatchTime.now() + 1
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            self.rangeSlider.trackHighlightTintColor = .gradientPurple
+            self.rangeSlider.thumbImage = UIImage(named: "star")
+            self.rangeSlider.highlightedThumbImage = UIImage(named: "star")
+        }
+        view.addSubview(rangeSlider)
+
+        rangeSlider2.addTarget(self, action: #selector(rangeSliderValueChanged(_:)), for: .valueChanged)
+        let time2 = DispatchTime.now() + 1
+        DispatchQueue.main.asyncAfter(deadline: time2) {
+            self.rangeSlider2.trackHighlightTintColor = .gradientPurple
+            self.rangeSlider2.thumbImage = UIImage(named: "star")
+            self.rangeSlider2.highlightedThumbImage = UIImage(named: "star")
+        }
+        view.addSubview(rangeSlider2)
 
         criticRatingLabel.text = "from Rotten Tomatoes and IMDb"
         criticRatingLabel.font = .systemFont(ofSize: 12)
         criticRatingLabel.textColor = .mediumGray
         criticRatingLabel.numberOfLines = 0
-        view.addSubview(criticRatingLabel)
+        contentView.addSubview(criticRatingLabel)
 
         friendRatingLabel.text = "Me, Lucy, and 4 others"
         friendRatingLabel.font = .systemFont(ofSize: 12)
         friendRatingLabel.textColor = .mediumGray
         friendRatingLabel.numberOfLines = 0
-        view.addSubview(friendRatingLabel)
+        contentView.addSubview(friendRatingLabel)
 
-        summaryView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(40)
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(summaryView.intrinsicContentSize.height)
-        }
+        commentView.backgroundColor = .movieWhite
 
-        reviewView.snp.makeConstraints { make in
-            make.top.equalTo(summaryView.snp.bottom).offset(37)
-            make.leading.trailing.equalToSuperview()
-        }
-
-//        commentView.backgroundColor = .orange
-//        commentView.layer.borderColor = UIColor.lightGray2.cgColor
-//        commentView.layer.borderWidth = 2
         commentSeparatorView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 2)
         commentSeparatorView.backgroundColor = .lightGray2
         commentView.addSubview(commentSeparatorView)
@@ -102,70 +114,32 @@ class MediaCardViewController: UIViewController {
         sendCommentButton.setImage(UIImage(named: "send"), for: .normal)
         commentView.addSubview(sendCommentButton)
 
-//        commentView.snp.makeConstraints { make in
-//            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-//            make.leading.trailing.equalToSuperview()
-//            make.height.equalTo(71)
-//        }
-//
-//        commentTextField.snp.makeConstraints { make in
-//            make.top.equalToSuperview().offset(16)
-//            make.leading.equalToSuperview().offset(28)
-//            make.trailing.equalToSuperview().inset(60)
-//            make.bottom.equalToSuperview().inset(16)
-//        }
-//
-//        sendCommentButton.snp.makeConstraints { make in
-//            make.width.height.equalTo(24)
-//            make.leading.equalTo(commentTextField.snp.trailing).offset(14)
-//            make.centerY.equalTo(commentTextField)
-//        }
-
-        commentsTableView = UITableView(frame: .zero, style: .plain)
+        commentsTableView = UITableView(frame: .zero)
         commentsTableView.backgroundColor = .none
         commentsTableView.dataSource = self
         commentsTableView.delegate = self
-        commentsTableView.isScrollEnabled = true
+        commentsTableView.isScrollEnabled = false
         commentsTableView.alwaysBounceVertical = false
         commentsTableView.rowHeight = UITableView.automaticDimension
         commentsTableView.estimatedRowHeight = 140
-        commentsTableView.layer.zPosition = -1
         commentsTableView.register(CommentTableViewCell.self, forCellReuseIdentifier: "commentsTableCell")
         commentsTableView.separatorStyle = .none
         view.addSubview(commentsTableView)
 
-        view.addSubview(rangeSlider)
-           rangeSlider.addTarget(self, action: #selector(rangeSliderValueChanged(_:)),
-                                 for: .valueChanged)
-           let time = DispatchTime.now() + 1
-           DispatchQueue.main.asyncAfter(deadline: time) {
-             self.rangeSlider.trackHighlightTintColor = .gradientPurple
-            self.rangeSlider.thumbImage = UIImage(named: "star")
-             self.rangeSlider.highlightedThumbImage = UIImage(named: "star")
-           }
+        setupConstraints()
 
-        view.addSubview(rangeSlider2)
-        rangeSlider2.addTarget(self, action: #selector(rangeSliderValueChanged(_:)),
-                              for: .valueChanged)
-        let time2 = DispatchTime.now() + 1
-        DispatchQueue.main.asyncAfter(deadline: time2) {
-          self.rangeSlider2.trackHighlightTintColor = .gradientPurple
-         self.rangeSlider2.thumbImage = UIImage(named: "star")
-          self.rangeSlider2.highlightedThumbImage = UIImage(named: "star")
-        }
-        
     }
 
-    private func setupHandleArea() {
-        view.addSubview(handleArea)
+    @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
+        let values = "(\(rangeSlider.lowerValue) \(rangeSlider.upperValue))"
+        print("Range slider value changed: \(values)")
+    }
 
-        handleIndicatorView.layer.backgroundColor = UIColor.lightGray4.cgColor
-        handleIndicatorView.layer.cornerRadius = 2
-        view.addSubview(handleIndicatorView)
+    private func setupConstraints() {
 
         handleArea.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
-            make.height.equalTo(75)
+            make.height.equalTo(30)
         }
 
         handleIndicatorView.snp.makeConstraints { make in
@@ -173,9 +147,28 @@ class MediaCardViewController: UIViewController {
             make.size.equalTo(handleIndicatorViewSize)
             make.top.equalToSuperview().offset(12)
         }
-    }
 
-    override func viewDidLayoutSubviews() {
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(handleArea.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(commentView.snp.top)
+        }
+
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView)
+        }
+
+        summaryView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(summaryView.intrinsicContentSize.height)
+        }
+
+        reviewView.snp.makeConstraints { make in
+            make.top.equalTo(summaryView.snp.bottom).offset(37)
+            make.leading.trailing.equalToSuperview()
+        }
 
         rangeSlider.snp.makeConstraints { make in
             make.top.equalTo(reviewView.snp.bottom).offset(50)
@@ -203,19 +196,38 @@ class MediaCardViewController: UIViewController {
             make.trailing.equalToSuperview().inset(20)
         }
 
+        commentView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(71 + view.safeAreaInsets.bottom)
+        }
+
+        commentTextField.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(28)
+            make.trailing.equalToSuperview().inset(60)
+            make.bottom.equalToSuperview().inset(16)
+        }
+
+        sendCommentButton.snp.makeConstraints { make in
+            make.width.height.equalTo(24)
+            make.leading.equalTo(commentTextField.snp.trailing).offset(14)
+            make.centerY.equalTo(commentTextField)
+        }
+
         commentsTableView.snp.makeConstraints { make in
             make.leading.equalTo(rangeSlider)
             make.trailing.equalTo(criticRatingLabel)
             make.top.equalTo(rangeSlider2.snp.bottom).offset(42)
             make.height.equalTo(100)
         }
+
     }
 
-
-    @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
-      let values = "(\(rangeSlider.lowerValue) \(rangeSlider.upperValue))"
-      print("Range slider value changed: \(values)")
+    override func viewDidLayoutSubviews() {
+        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height * 1.5)
     }
+
 
 }
 
@@ -233,3 +245,4 @@ extension MediaCardViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
 }
+
