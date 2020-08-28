31c478974ff6d069621c529f9f5afe54d57bcc20
haiyingweng <haiyingweng@gmail.com>
2020-08-27 23:52:27 -0400

    Add list preview

--- Flick/Cells/DiscoverSearchResultTableViewCell.swift
+++ Flick/Cells/DiscoverSearchResultTableViewCell.swift
@@ -11,16 +11,17 @@ import UIKit
 class DiscoverSearchResultTableViewCell: UITableViewCell {
 
     // MARK: - Private View Vars
+    private let listPreviewView = ListPreviewView()
+    private let iconImageView = UIImageView()
     private let infoStackView = UIStackView()
-    private let subtitleStackView = UIStackView()
-
     private let resultImageView =  UIImageView()
     private let subtitleLabel = UILabel()
+    private let subtitleStackView = UIStackView()
     private let titleLabel = UILabel()
-    private let iconImageView = UIImageView()
 
     // MARK: - Private Data Vars
     private let circleImageViewSize = CGSize(width: 36, height: 36)
+    private let listPreviewSize = CGSize(width: 37, height: 48)
     private let posterImageViewSize = CGSize(width: 36, height: 54)
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -42,9 +43,10 @@ class DiscoverSearchResultTableViewCell: UITableViewCell {
         subtitleStackView.spacing = 5
         infoStackView.addArrangedSubview(subtitleStackView)
 
+        let iconSize = CGSize(width: 14, height: 14)
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.snp.makeConstraints { make in
-            make.size.equalTo(CGSize(width: 14, height: 14))
+            make.size.equalTo(iconSize)
         }
         subtitleStackView.addArrangedSubview(iconImageView)
 
@@ -58,6 +60,9 @@ class DiscoverSearchResultTableViewCell: UITableViewCell {
         resultImageView.layer.cornerRadius = 4
         contentView.addSubview(resultImageView)
 
+        listPreviewView.isHidden = true
+        contentView.addSubview(listPreviewView)
+
         setupConstraints()
     }
 
@@ -69,17 +74,22 @@ class DiscoverSearchResultTableViewCell: UITableViewCell {
         titleLabel.text = titleText
         switch searchType {
         case .lists:
+            resultImageView.isHidden = true
             iconImageView.isHidden = true
-            subtitleLabel.text = "by Lucy Xu"
+            listPreviewView.isHidden = false
+            setupConstraintsForListPreview()
+            subtitleLabel.text = "by Lucy Xu" // Temp
         case .movies:
             updateConstraintsForPoster()
+            resultImageView.image = UIImage(named: "dunkirk") // Temp
             iconImageView.image = UIImage(named: "film")
         case .people:
             updateConstraintsForCircleImage()
             resultImageView.layer.cornerRadius = circleImageViewSize.width / 2
             iconImageView.isHidden = true
-            subtitleLabel.text = "3 mutual friends"
+            subtitleLabel.text = "3 mutual friends" // Temp
         case .shows:
+            resultImageView.image = UIImage(named: "dunkirk") // Temp
             updateConstraintsForPoster()
             iconImageView.image = UIImage(named: "tv")
         case .tags:
@@ -116,6 +126,21 @@ class DiscoverSearchResultTableViewCell: UITableViewCell {
     private func updateConstraintsForCircleImage() {
         resultImageView.snp.updateConstraints { update in
             update.size.equalTo(circleImageViewSize)
+        }
+    }
+
+    private func setupConstraintsForListPreview() {
+        listPreviewView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24)
+            make.size.equalTo(listPreviewSize)
+            make.top.bottom.equalToSuperview().inset(8)
+            make.centerY.equalToSuperview()
+        }
+
+        infoStackView.snp.makeConstraints { remake in
+            remake.leading.equalTo(listPreviewView.snp.trailing).offset(10)
+            remake.trailing.equalToSuperview()
+            remake.centerY.equalToSuperview()
         }
     }
 }
