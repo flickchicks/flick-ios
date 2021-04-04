c8aa326df787ae2e39ee25e8c5826b910aec9a9a
lucyxubroad <llx2@cornell.edu>
2021-04-03 22:58:08 -0400

    Update AddCollaborator

--- Telie/List/AddCollaboratorViewController.swift
+++ Telie/List/AddCollaboratorViewController.swift
@@ -23,20 +23,25 @@ class AddCollaboratorViewController: UIViewController {
         color: .gradientPurple
     )
     private let subtitleLabel = UILabel()
-    private let noFriendsLabel = UILabel()
+    private var currentSearchText = ""
 
     // MARK: - Private Data Vars
     private var allFriends: [UserProfile] = []
     private let collaboratorCellHeight = 57
     private var collaborators: [UserProfile]
     private var friends: [UserProfile] = []
+    private var listFriends: [UserProfile] = []
     private var owner: UserProfile
+    private let addCollaboratorButton = UIButton()
+
+    private var editMode = "Remove"
 
     weak var listSettingsDelegate: ListSettingsDelegate?
 
     init(owner: UserProfile, collaborators: [UserProfile]) {
         self.owner = owner
         self.collaborators = [owner] + collaborators
+        self.listFriends = [owner] + collaborators
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -62,39 +67,9 @@ class AddCollaboratorViewController: UIViewController {
         collaboratorsTableView.alwaysBounceVertical = false
         collaboratorsTableView.showsVerticalScrollIndicator = false
         collaboratorsTableView.register(EditUserTableViewCell.self, forCellReuseIdentifier: EditUserTableViewCell.reuseIdentifier)
+        collaboratorsTableView.register(InviteUserTableViewCell.self, forCellReuseIdentifier: InviteUserTableViewCell.reuseIdentifier)
         collaboratorsTableView.separatorStyle = .none
         view.addSubview(collaboratorsTableView)
-
-        inviteTitleLabel.text = "Invite"
-        inviteTitleLabel.textColor = .black
-        inviteTitleLabel.font = .boldSystemFont(ofSize: 18)
-        view.addSubview(inviteTitleLabel)
-
-        inviteSearchBar.placeholder = "Search friends"
-        inviteSearchBar.delegate = self
-        view.addSubview(inviteSearchBar)
-
-        inviteCollaboratorsTableView = UITableView(frame: .zero, style: .plain)
-        inviteCollaboratorsTableView.dataSource = self
-        inviteCollaboratorsTableView.delegate = self
-        inviteCollaboratorsTableView.allowsMultipleSelection = true
-        inviteCollaboratorsTableView.isScrollEnabled = true
-        inviteCollaboratorsTableView.alwaysBounceVertical = false
-        inviteCollaboratorsTableView.showsVerticalScrollIndicator = false
-        inviteCollaboratorsTableView.register(EditUserTableViewCell.self, forCellReuseIdentifier: EditUserTableViewCell.reuseIdentifier)
-        inviteCollaboratorsTableView.separatorStyle = .none
-        inviteCollaboratorsTableView.keyboardDismissMode = .onDrag
-
-        if friends.isEmpty {
-            view.addSubview(spinner)
-            spinner.startAnimating()
-        }
-
-        noFriendsLabel.text = "Stop telling your friends what to watch when they always forget... \nTell them to join Telie!"
-        noFriendsLabel.textColor = .darkBlue
-        noFriendsLabel.numberOfLines = 0
-        noFriendsLabel.font = .systemFont(ofSize: 12)
-        noFriendsLabel.textAlignment = .center
 
         setupConstraints()
 
@@ -103,22 +78,21 @@ class AddCollaboratorViewController: UIViewController {
             DispatchQueue.main.async {
                 self.allFriends = friends
                 self.friends = friends
-                self.setupFriendsView()
+                self.listFriends = self.editMode == "Remove" ? self.collaborators : friends
                 self.spinner.stopAnimating()
             }
         }
+
+    }
+
+    @objc func addCollaboratorButtonPressed() {
+
     }
 
     private func setupConstraints() {
         let collaboratorsTitleLabelSize = CGSize(width: 117, height: 22)
         let horizontalPadding = 24
-        let inviteTitleLabelSize = CGSize(width: 48, height: 22)
-        let noFriendsSectionViewHeight = 3 * collaboratorCellHeight
-        let roundButtonSize = CGSize(width: 84, height: 40)
         let verticalPadding = 36
-
-        let collaboratorsTableViewHeight = min(collaborators.count, 3) * collaboratorCellHeight
-        let containerHeight = noFriendsSectionViewHeight + collaboratorsTableViewHeight + Int(roundButtonSize.height) + 287
 
         collaboratorsTitleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(verticalPadding)
@@ -136,47 +110,7 @@ class AddCollaboratorViewController: UIViewController {
             make.leading.equalTo(collaboratorsTitleLabel)
             make.trailing.equalToSuperview().inset(horizontalPadding)
             make.top.equalTo(subtitleLabel.snp.bottom).offset(20)
-            make.height.equalTo(collaboratorsTableViewHeight)
-        }
-
-        inviteTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(collaboratorsTableView.snp.bottom).offset(20.5)
-            make.leading.equalTo(collaboratorsTitleLabel)
-            make.size.equalTo(inviteTitleLabelSize)
-        }
-
-        inviteSearchBar.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(collaboratorsTableView)
-            make.height.equalTo(40)
-            make.top.equalTo(inviteTitleLabel.snp.bottom).offset(18)
-        }
-
-        spinner.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(inviteSearchBar.snp.bottom).offset(20)
-        }
-    }
-
-    private func setupFriendsView() {
-        let inviteHeight = 3 * collaboratorCellHeight
-        if friends.count > 0 {
-            view.addSubview(inviteCollaboratorsTableView)
-
-            inviteCollaboratorsTableView.snp.makeConstraints { make in
-                make.leading.equalTo(collaboratorsTitleLabel)
-                make.trailing.equalTo(subtitleLabel)
-                make.height.equalTo(inviteHeight)
-                make.top.equalTo(inviteSearchBar.snp.bottom).offset(17)
-            }
-        } else {
-            view.addSubview(noFriendsLabel)
-
-            noFriendsLabel.snp.makeConstraints { make in
-                make.leading.equalTo(collaboratorsTitleLabel)
-                make.trailing.equalTo(subtitleLabel)
-                make.top.equalTo(inviteSearchBar.snp.bottom).offset(17)
-                make.height.equalTo(inviteHeight)
-            }
+            make.bottom.equalToSuperview()
         }
     }
 
@@ -193,8 +127,35 @@ class AddCollaboratorViewController: UIViewController {
 
 extension AddCollaboratorViewController: UITableViewDelegate, UITableViewDataSource {
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .white
+        let headerLabel = UILabel()
+        headerLabel.textColor = .darkBlueGray2
+        headerLabel.font = .boldSystemFont(ofSize: 12)
+//        headerLabel.text = section == 0 ? "Invite" : "Collaborators"
+        if section == 0 {
+            headerLabel.text = "Invite"
+        } else {
+            if editMode == "Remove" {
+                headerLabel.text = "Collaborators"
+            } else {
+                headerLabel.text = "Friends"
+            }
+        }
+        headerView.addSubview(headerLabel)
+        headerLabel.snp.makeConstraints { make in
+            make.leading.bottom.equalToSuperview()
+        }
+        return headerView
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableView == collaboratorsTableView ? collaborators.count : friends.count
+        return section == 0 ? 1 : listFriends.count
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -202,34 +163,42 @@ extension AddCollaboratorViewController: UITableViewDelegate, UITableViewDataSou
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: EditUserTableViewCell.reuseIdentifier, for: indexPath) as? EditUserTableViewCell else { return UITableViewCell() }
-        if tableView == collaboratorsTableView {
-            let collaborator = collaborators[indexPath.row]
+        if indexPath.section == 1 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: EditUserTableViewCell.reuseIdentifier, for: indexPath) as? EditUserTableViewCell else { return UITableViewCell() }
+            let collaborator = listFriends[indexPath.row]
             cell.configureForRemove(user: collaborator, isOwner: owner.id == collaborator.id)
             cell.delegate = self
+            return cell
         } else {
-            let friend = friends[indexPath.row]
-            let isAdded = collaborators.contains { $0.id == friend.id }
-            cell.configureForAdd(user: friend, isAdded: isAdded)
-            cell.delegate = self
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: InviteUserTableViewCell.reuseIdentifier, for: indexPath) as? InviteUserTableViewCell else { return UITableViewCell() }
+            cell.configure(with: self)
+            return cell
         }
-        return cell
     }
 }
 
 extension AddCollaboratorViewController: UISearchBarDelegate {
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if allFriends.count > 0 {
-            if searchText == "" {
-                friends = allFriends
-            } else {
-                friends = allFriends.filter {
-                    "\($0.name)".contains(searchText) || $0.username.contains(searchText)
+        if searchText.isEmpty && !currentSearchText.isEmpty {
+            editMode = "Remove"
+            listFriends = collaborators
+        } else if searchText.isEmpty && currentSearchText.isEmpty {
+            editMode = "Invite"
+            listFriends = allFriends
+        } else {
+            if allFriends.count > 0 {
+                if searchText == "" {
+                    listFriends = allFriends
+                } else {
+                    listFriends = allFriends.filter {
+                        "\($0.name)".contains(searchText) || $0.username.contains(searchText)
+                    }
                 }
             }
-            inviteCollaboratorsTableView.reloadData()
         }
+        currentSearchText = searchText
+        collaboratorsTableView.reloadData()
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
