dbadb7d320c9c60f124e3636dcdb3605f1c4c55f
asz33@cornell.edu <asz33@cornell.edu>
2022-04-15 20:32:08 -0400

    Create view controller

---
+++ Telie/Controllers/WatchProgressListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WatchProgressListViewController.swift
+//  Telie
+//
+//  Created by Alanna Zhou on 4/15/22.
+//  Copyright © 2022 Telie. All rights reserved.
+//
+
+import UIKit
+
+class WatchProgressListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Watch Progress"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
